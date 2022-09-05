35c38963ddb379ab33fe7ca10e53e68cc8db6259
tmaleckiplacester <tmalecki@placester.com>
2022-09-05 17:19:25 +0200

    Automated TC-1 - Enter Correct E-mail and correct Password

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
+    baseUrl: 'https://qa-recruitment.codiblycoders.com/'
   },
 });
