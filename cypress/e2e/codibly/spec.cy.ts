97489d388cec4adcec8503c8b3cf62cbcbeb3fff
tmaleckiplacester <tmalecki@placester.com>
2022-09-05 17:19:08 +0200

    Added Spec BaseUrl

--- cypress/e2e/codibly/spec.cy.ts
+++ cypress/e2e/codibly/spec.cy.ts
@@ -1,5 +1,28 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+beforeEach(() => {
+  cy.visit('/')
+
+});
+
+before(() => {
+  cy.fixture('codibly').then((fixture) => {
+  this.data = fixture;
+  });
+
+  cy.intercept('POST','https://run.mocky.io/v3/b08050cb-14d8-44b2-a8ab-a366ecead596').as('logIn')
+});
+
+
+describe('Enter correct e-mail and correct password', () => {
+  it('Successful Log in', () => {
+    cy.get('input[name=email]').first().type(this.data.email)
+    cy.get('input[name=password]').first().type(this.data.password)
+    cy.get('button[type=submit]').contains('Log in').click()
+
+    cy.wait('@logIn').then((interception) => {
+      expect(interception.response).property('statusCode').to.equal(200)
+      expect(interception.response.body).to.have.property('token')
+    })
+
+    cy.get('div[data-testid=app__login-section]').contains('Successfuly logged in').should('be.visible')
   })
 })
